4dac61e660147f4408ff7e0e4729627690911fd6
Carl1975 <carl.snaylam@gmail.com>
2023-02-10 21:15:25 +0000

    Link to index Albums Poll page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-export default function Home() {
+import Link from "next/link";
+
+export default function HomePage() {
   return (
-    <div className="w-full py-10 bg-red-100">Home</div>
+    <div className="w-full py-10 bg-red-100">Home
+      <div>
+        <Link href="/albums">
+            Album Poll
+        </Link>
+      </div>
+    </div>
   )
 }
