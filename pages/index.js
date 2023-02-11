6ab09835fb7b020235009464a52fb5c42a333822
Carl1975 <carl.snaylam@gmail.com>
2023-02-11 08:55:34 +0000

    remove onClick link to Albums Poll page

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,12 @@
 import Link from "next/link";
-/* import { useRouter } from "next/router"; */
 
 export default function HomePage() {
-/*     const router = useRouter(); */
-  
-    /* const handleClick = () => {
-       /* make API call example */
-     /*  router.push("/albums"); */
-   /*  }  */
-
   return (
-    <div className="w-full py-10 bg-red-100">Home
-
-      {/* <button className="bg-blue-500 px-2 py-1 rounded"
-      onClick={handleClick}>
-        Albums Poll
-      </button> */} 
-
+    <div className="w-full py-10 bg-green-100">
+      Home
       <Link href="/albums">
         Albums Poll
       </Link>
-
     </div>
   )
 }
