f5a4aff92ce37e19ada3c6cd559fa4e8eace3573
Carl1975 <carl.snaylam@gmail.com>
2023-02-11 05:00:24 +0000

    using Link to Albums Poll page instead of onClick event

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,26 @@
-import { useRouter } from "next/router";
+import Link from "next/link";
+/* import { useRouter } from "next/router"; */
 
 export default function HomePage() {
-  const router = useRouter();
-
-  const handleClick = () => {
-
-  /* make API call example */
-    router.push("/albums");
-  }
+/*     const router = useRouter(); */
+  
+    /* const handleClick = () => {
+       /* make API call example */
+     /*  router.push("/albums"); */
+   /*  }  */
 
   return (
     <div className="w-full py-10 bg-red-100">Home
 
-      <button className="bg-blue-500 px-2 py-1 rounded"
+      {/* <button className="bg-blue-500 px-2 py-1 rounded"
       onClick={handleClick}>
         Albums Poll
-      </button>
+      </button> */} 
+
+      <Link href="/albums">
+        Albums Poll
+      </Link>
+
     </div>
   )
 }
