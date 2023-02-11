396161fa6b295bc15acc701a182483c06066aea7
Carl1975 <carl.snaylam@gmail.com>
2023-02-11 04:59:57 +0000

    using Link to home page instead of onClick event

--- pages/albums.js
+++ pages/albums.js
@@ -1,20 +1,26 @@
-import { useRouter } from "next/router";
+import Link from "next/link";
+/* import { useRouter } from "next/router"; */
 
-export default function AlbumPage() {
-  const router = useRouter();
-
-  const handleClick = () => {
-     /* make API call example */
-    router.push("/");
-  }
+export default function HomePage() {
+/*     const router = useRouter(); */
+  
+    /* const handleClick = () => {
+       /* make API call example */
+     /*  router.push("/albums"); */
+   /*  }  */
 
   return (
     <div className="w-full py-10 bg-green-100">Albums Poll
 
-      <button className="bg-blue-500 px-2 py-1 rounded"
+      {/* <button className="bg-blue-500 px-2 py-1 rounded"
       onClick={handleClick}>
+        Albums Poll
+      </button> */} 
+
+      <Link href="/">
         Home
-      </button>
+      </Link>
+
     </div>
   )
 }
