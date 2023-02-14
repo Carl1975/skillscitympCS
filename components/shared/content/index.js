e0b0ee65fad870d9571f5d96163b55ec9f854a88
Carl1975 <carl.snaylam@gmail.com>
2023-02-14 16:04:15 +0000

    Content code added

---
+++ components/shared/content/index.js
@@ -0,0 +1,9 @@
+import AlbumsGrid from "@/components/albums/albums-grid";
+export default function Content(){
+    return (
+        <div className="w-full max-w-6xl mx-auto mt-8">
+        <AlbumsGrid />
+      </div>
+
+    )
+}
