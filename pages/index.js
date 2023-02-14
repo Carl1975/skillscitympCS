15cbfb30afd049c2085f6f37ec710ad2dffe273a
Carl1975 <carl.snaylam@gmail.com>
2023-02-14 16:02:47 +0000

    Footer code added

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
-import Link from "next/link";
+
+import Footer from "@/components/shared/footer";
+import Header from "@/components/shared/header";
 
 export default function HomePage() {
   return (
-    <div className="w-full py-10 bg-green-100">
-      Home
-      <Link href="/albums">
-        Albums Poll
-      </Link>
+      <div className="w-full">
+        <Header name="Home" />
+
+      <div className="w-full max-w-6xl mx-auto mt-8">
+      <Footer href="/albums" title="Albums"/>
+      </div>
     </div>
   )
-}
+  }
+
+
+
