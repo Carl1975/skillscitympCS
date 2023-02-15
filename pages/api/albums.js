4fd81e8f7c6c3129aa5a3fe6c60bd71c8422f667
Carl1975 <carl.snaylam@gmail.com>
2023-02-15 08:02:01 +0000

    modified to include ref to imgUrl

--- pages/api/albums.js
+++ pages/api/albums.js
@@ -2,23 +2,28 @@ export default function handler(req, res) {
     const albums = [
         {
             title: "Hunky Dory",
-            artist: "David Bowie"
+            artist: "David Bowie",
+            imgUrl: "/assets/Hunky_Dory_.jpg"
         },
         {
             title: "Technique",
-            artist: "New Order"
+            artist: "New Order",
+            imgUrl: "/assets/Technique.jpg"
         },
         {
             title: "Stone Roses",
-            artist: "The Stone Roses"
+            artist: "The Stone Roses",
+            imgUrl: "/assets/Stone_Roses_.jpg"
         },
         {
             title: "Legend",
-            artist: "Bob Marley"
+            artist: "Bob Marley",
+            imgUrl: "/assets/Legend_.jpg"
         },
         {
             title: "Abbey Road",
-            artist: "The Beatles"
+            artist: "The Beatles",
+            imgUrl: "/assets/Abbey_Road_.jpg"
         }
     ];
     res.json(albums);
