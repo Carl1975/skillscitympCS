792d005c9aae061965c47972d51ece52d094fe3f
Carl1975 <carl.snaylam@gmail.com>
2023-02-15 07:23:35 +0000

    modified to import content

--- pages/albums.js
+++ pages/albums.js
@@ -1,15 +1,21 @@
-/* import AlbumsGrid from "@/components/albums/albums-grid/album-item"; */
-/* import AlbumsGrid from "@/components/albums/albums-grid"; */
-import Content from "@/components/shared/content";
+/* import AlbumsGrid from "@/components/albums/albums-grid/album-item";  */
+import AlbumsGrid from "@/components/albums/albums-grid";
+import Content from "@/components/shared/content"; 
 import Footer from "@/components/shared/footer";
 import Header from "@/components/shared/header";
 
 export default function AlbumPage() {
   return (
       <div className="w-full bg-red-100 max-w-6xl mx-auto mt-8">
-        <Header name="Album" />
+        <Header name="Albums" />
+
+        <Content>
+          <div className="w-full max-w-6xl mx-auto mt-8">
+        <AlbumsGrid />
+          </div>
+        </Content>
   
-     <Content></Content>
+      
 
       <div className="w-full max-w-6xl mx-auto mt-8">
       <Footer href="/" title="Home"/>
