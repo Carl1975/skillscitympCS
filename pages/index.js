56d9bdfb792016328b1a817f1b00319d0a579666
Carl1975 <carl.snaylam@gmail.com>
2023-02-15 07:24:22 +0000

    modified to import topics, hero, and content

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,29 @@
-
+import Topics from "@/components/home/topics";
+import Hero from "@/components/home/hero";
+import Content from "@/components/shared/content";
 import Footer from "@/components/shared/footer";
 import Header from "@/components/shared/header";
 
 export default function HomePage() {
   return (
-      <div className="w-full">
+    <div className="w-full">
         <Header name="Home" />
 
-      <div className="w-full max-w-6xl mx-auto mt-8">
-      <Footer href="/albums" title="Albums"/>
-      </div>
+        <Hero imgUrl="/assets/profilepicrev1.jpg"
+        title="Carl Snaylam"
+        subtitle="Aspiring Software Engineer"
+        /> 
+
+        <Content>
+          <div className="w-full flex flex-col">
+          <Topics />
+
+          </div>
+          
+        </Content>
+        <Footer href="/albums" title="Albums"/>
     </div>
-  )
+    )
   }
 
 
