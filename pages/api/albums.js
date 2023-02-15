ea54500672a0686d9a5cd03b0174a6598616c9c2
Carl1975 <carl.snaylam@gmail.com>
2023-02-15 18:59:08 +0000

    id added

--- pages/api/albums.js
+++ pages/api/albums.js
@@ -1,26 +1,31 @@
 export default function handler(req, res) {
     const albums = [
         {
+            id: "1",
             title: "Hunky Dory",
             artist: "David Bowie",
             imgUrl: "/assets/Hunky_Dory_.jpg"
         },
         {
+            id: "2",
             title: "Technique",
             artist: "New Order",
             imgUrl: "/assets/Technique.jpg"
         },
         {
+            id: "3",
             title: "Stone Roses",
             artist: "The Stone Roses",
             imgUrl: "/assets/Stone_Roses_.jpg"
         },
         {
+            id: "4",
             title: "Legend",
             artist: "Bob Marley",
             imgUrl: "/assets/Legend_.jpg"
         },
         {
+            id: "5",
             title: "Abbey Road",
             artist: "The Beatles",
             imgUrl: "/assets/Abbey_Road_.jpg"
