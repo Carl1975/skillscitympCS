e6dc9fc67ba3c2f5e129250a3e16dedf1410b759
Carl1975 <carl.snaylam@gmail.com>
2023-02-15 07:22:49 +0000

    Content function created

--- components/shared/content/index.js
+++ components/shared/content/index.js
@@ -1,8 +1,7 @@
-import AlbumsGrid from "@/components/albums/albums-grid";
-export default function Content(){
+export default function Content({ children }){
     return (
-        <div className="w-full max-w-6xl mx-auto mt-8">
-        <AlbumsGrid />
+      <div className="w-full max-w-6xl mx-auto">
+          {children}
       </div>
 
     )
