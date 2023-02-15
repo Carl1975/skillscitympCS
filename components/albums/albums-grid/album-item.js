4bb8c0f04d8af383b01a52093ae9b99c77c88b72
Carl1975 <carl.snaylam@gmail.com>
2023-02-15 08:03:36 +0000

    updated to include img tag

--- components/albums/albums-grid/album-item.js
+++ components/albums/albums-grid/album-item.js
@@ -1,6 +1,7 @@
 export default function AlbumItem({ album }) {
   return (
     <div className="bg-green-100">
+      <img src={album.imgUrl} />
       <p>{album.title}</p>
       <p>{album.artist}</p>
     </div>
