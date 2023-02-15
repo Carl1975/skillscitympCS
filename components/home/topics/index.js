836df799ce5a662207f85c75af3571557419b78e
Carl1975 <carl.snaylam@gmail.com>
2023-02-15 07:21:47 +0000

    Array of topics imported

---
+++ components/home/topics/index.js
@@ -0,0 +1,21 @@
+import topics from "./topics";
+
+export default function Topics(){
+    return(
+        <div className="w-full flex flex-col mt-5 mb-10">
+            <h3 className="font-medium text-2xl">
+            Topics covered on the bootcamp
+            </h3>
+            <div>
+                <ul className="list-disc">
+                    {topics.map((topics,index)=> {
+                        return <li key={index}>{topics}</li>
+                    }
+                    )}
+                    
+                </ul>
+            </div>
+        </div>
+    )
+}
+
