267a2191735e183443772c9fa8c0eac2b1a5f3a2
Carl1975 <carl.snaylam@gmail.com>
2023-02-15 08:02:47 +0000

    Key index used for unique ref

--- components/albums/albums-grid/index.js
+++ components/albums/albums-grid/index.js
@@ -18,8 +18,8 @@ export default function AlbumsGrid() {
 
   return (
     <div className="w-full grid grid-cols-5 gap-4">
-      {albums.map((album) => (
-        <AlbumItem album={album} />
+      {albums.map((album, index) => (
+        <AlbumItem Key={index} album={album} />
 
       ))}
     </div>
