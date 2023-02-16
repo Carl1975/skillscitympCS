0f868b37c6872dd042edecca9713d7f777e70676
Carl1975 <carl.snaylam@gmail.com>
2023-02-16 09:07:50 +0000

    modified to remove page texy

--- components/shared/header/index.js
+++ components/shared/header/index.js
@@ -1,7 +1,7 @@
 export default function Header({ name }) {
   return (
     <div className="bg-blue-100 w-full max-w-6xl mx-auto py-4 px-4">
-      <h1 className="font-bold text-xl">{name} Page</h1>
+      <h1 className="font-bold text-xl">{name}</h1>
     </div>
 
   );
