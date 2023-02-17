f0ef5f303e6ff24af2a9750ba80203e9a0b06520
Carl1975 <carl.snaylam@gmail.com>
2023-02-17 05:20:19 +0000

    db all albums query to postgres

--- pages/api/album-by-id.js
+++ pages/api/album-by-id.js
@@ -1,52 +1,20 @@
-export default function handler(req, res) {
+import sql from "@/utils/postgres";
+
+export default async function handler(req, res) {
     const id = req.query.id;
-    const albums = [
-        {
-            id: "1",
-            title: "Hunky Dory",
-            artist: "David Bowie",
-            imgUrl: "/assets/Hunky_Dory_.jpg",
-            linkToPurchase: "https://www.amazon.co.uk/Hunky-Dory-Remastered-Version-VINYL/dp/B01AJZ8ELC/ref=sr_1_3?crid=2PIGWZAT3XMIO&keywords=hunky+dory+vinyl&qid=1676461875&sprefix=hunly%2Caps%2C95&sr=8-3"
-        },
-        {
-            id: "2",
-            title: "Technique",
-            artist: "New Order",
-            imgUrl: "/assets/Technique.jpg",
-            /* linkToPurchase: "" */
-        },
-        {
-            id: "3",
-            title: "Stone Roses",
-            artist: "The Stone Roses",
-            imgUrl: "/assets/Stone_Roses_.jpg",
-           /*  linkToPurchase: "" */
-        },
-        {
-            id: "4",
-            title: "Legend",
-            artist: "Bob Marley",
-            imgUrl: "/assets/Legend_.jpg",
-  /*           linkToPurchase: "" */
-        },
-        {
-            id: "5",
-            title: "Abbey Road",
-            artist: "The Beatles",
-            imgUrl: "/assets/Abbey_Road_.jpg",
-            /* linkToPurchase: "" */
-        }
-    ];
-    const foundAlbum = albums.find((album) => {
-        if (album.id === id) {
-            return true;
-        }
-        else {
-            return false;
-        }
 
-    })
+    const albums = await sql`
+        select * from albums
+        where id = ${id}
+    `;
+    if (albums.length < 1) {
+        res.status(404).json ({message: "album not found"});
+        return;
+    }
+    const foundAlbum = albums[0];
 
-    res.json({album: foundAlbum});
+    res.json({album: foundAlbum})
 
-}
+   }
+
+    
