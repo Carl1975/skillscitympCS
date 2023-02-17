11ad8e325df6019a78a63027a3a4671748529ed7
Carl1975 <carl.snaylam@gmail.com>
2023-02-17 08:29:10 +0000

    link on image  to single album page

--- components/albums/albums-grid/album-item.js
+++ components/albums/albums-grid/album-item.js
@@ -15,11 +15,10 @@ export default function AlbumItem({ album }) {
 
   return (
     <div className="border rounded-lg p-4">
-      <img src={album.img_url} className="rounded-lg"/>
-     {/*  <Link href={`/single-album/${album.id}`}></Link> */}
-      <Link href={`/single-album/${album.id}`} className="text-xl medium-font">
-      {album.title}
+      <Link href={`/single-album/${album.id}`}>
+        <img src={album.img_url} className="rounded-lg"/>
       </Link>
+      <p className="text-xl medium-font">{album.title}</p>
       <p className="text-gray-700">{album.artist}</p>
       <button
             onClick={handleLike}
