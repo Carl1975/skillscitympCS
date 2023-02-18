c2ee52c5f6a42ee88660f6dd70cc752a72530f01
Carl1975 <carl.snaylam@gmail.com>
2023-02-18 03:38:48 +0000

    added contact page

---
+++ pages/contact.js
@@ -0,0 +1,32 @@
+import Header from "@/components/shared/header";
+import Content from "@/components/shared/content";
+import Footer from "@/components/shared/footer";
+
+
+export default function Contact(){
+    return(
+        <div className="w-ful">
+            <Header name="Contact" />
+
+            <Content>
+                <Form>
+                    <input 
+                    
+                    
+                    />
+                    <input 
+                    
+                    
+                    />
+                    <textarea 
+                    
+                    
+                    />
+                </Form>
+            </Content>
+
+            <Footer title="Home" href="/" />
+
+        </div>
+    )
+}
